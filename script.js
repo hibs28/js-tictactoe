f05814e89519c62288526b173992fe64a4c00131
Tech-A06 <hibah28@live.co.uk>
2018-11-30 01:12:57 +0000

    incomplete check player turn function

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let clicked = false;
 let squareNumber;
 let player = 1;
 const playerTurnText = document.getElementsByClassName("playerTurn");
+let playerOneTurn = 0;
+let playerTwoTurn = 0;
 let playerOneTurnArr = [];
 let playerTwoTurnArr = [];
-
-
 
 const winCondition = [
   [0, 1, 2],
@@ -23,38 +23,53 @@ const winCondition = [
   [2, 4, 6],
 ];
 
-
+//==================MAKING THE GRID CLICKABLE==========================
 for (let i = 0; i < squareArr.length; i++) {
   squareArr[i].getAttribute("data-num");
   squareArr[i].setAttribute("class", "clear");
   squareArr[i].addEventListener("click", () => {
     clicked = true;
     checkClicked(i);
+    console.log("x array" + playerOneTurnArr);
     //clicked(i);
   });
-
 }
-
+//==================CHECK CLICK==========================
 const checkClicked = (i) => {
   if (clicked === true) {
     if (squareArr[i].className === "clear" && player === 1) {
-      addPlayerOne(i);
+      playerOneTurn += 1;
+      checkPlayerOneTurn(i);
       player = 2;
-      playerTurnText.innerHTML = "It is O's turn";
+
     }
     else if (squareArr[i].className === "clear" && player === 2) {
       addPlayerTwo(i);
       player = 1;
-      playerTurnText.innerHTML = "It is X's turn";
+
     }
     else {
       alert("Choose another box");
     }
-
   }
-
 }
+//==================PLAYER TURNS=============================
+const checkPlayerOneTurn = (i) => {
+  if (playerOneTurn >= 3) {
+    for (let j = 0; j < playerOneTurnArr.length; j++) {
+      for (let k = 0; k < winCondition.length; k++) {
+        // if (playerOneTurnArr[j].value === winCondition[k].value) {
+        //   alert("PlayerOne Wins");
+        // }
 
+        console.log(winCondition[k].value);
+      }
+    }
+  } else {
+    addPlayerOne(i);
+  }
+}
+//=================STORING PLAYER'S CLICKS===============================
 const addPlayerOne = (i) => {
   squareArr[i].setAttribute("class", "X");
   squareArr[i].innerHTML = "X";
@@ -65,26 +80,16 @@ const addPlayerTwo = (i) => {
   squareArr[i].setAttribute("class", "O");
   squareArr[i].innerHTML = "O";
   playerTwoTurnArr.push(i);
-
 }
-// const addPlayerTwo = () => {
-//   for (let i = 0; i < squareArr.length; i++) {
-//     squares[dotSequence[i]].classList.add("X");
-//   }
-// }
+//==================RESET================================================
+resetButton.addEventListener("click", () => {
+  clearSquare();
+});
 
 const clearSquare = () => {
   for (let i = 0; i < squareArr.length; i++) {
     squareArr[i].removeAttribute("class");
     squareArr[i].innerHTML = "";
     squareArr[i].setAttribute("class", "clear");
-
-
-    //    squareArr[i].setAttribute("class", "clear");
   }
 }
-
-resetButton.addEventListener("click", () => {
-  clearSquare();
-
-});
